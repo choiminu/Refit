10f854aac90a5424c1d0d7a74fd25b871e0cbf36
choiminu <alsdn4590@gmail.com>
2025-07-22 17:58:57 +0900

    docs : 회원가입 API 명세 설명 수정

--- src/main/java/com/refit/api/user/UserController.java
+++ src/main/java/com/refit/api/user/UserController.java
@@ -5,6 +5,7 @@ import com.refit.api.user.dto.UserSignupResponse;
 import com.refit.domain.user.entity.type.LoginType;
 import com.refit.domain.user.service.UserService;
 import com.refit.global.api.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,6 +19,7 @@ public class UserController {
 
     private final UserService userService;
 
+    @Operation(summary = "회원가입", description = "이메일, 비밀번호를 입력받아 신규 사용자를 등록합니다.")
     @PostMapping
     public ApiResponse<UserSignupResponse> signup(@RequestBody UserSignupRequest request) {
         UserSignupResponse response = userService.signup(request, LoginType.LOCAL);
