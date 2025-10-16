b20e80c00d6477539e4ad6f2e9b4de5cc70233f3
choiminu <alsdn4590@gmail.com>
2025-10-16 20:10:36 +0900

    feat(user): UserMapper 클래스 생성

---
+++ src/main/java/com/refit/user/application/service/UserMapper.java
@@ -0,0 +1,10 @@
+package com.refit.user.application.service;
+
+import com.refit.user.application.dto.UserSignupRequest;
+import com.refit.user.domain.User;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+    User signupRequestToEntity(UserSignupRequest req);
+}
